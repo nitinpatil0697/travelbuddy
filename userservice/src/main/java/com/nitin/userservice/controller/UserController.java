bbeba6184211e006c990f1253b78a4b4d94bab32
Nitin Patil <pnitin372@gmail.com>
2024-10-02 13:26:26 +0530

    User CRUD operation Implemented

--- userservice/src/main/java/com/nitin/userservice/controller/UserController.java
+++ userservice/src/main/java/com/nitin/userservice/controller/UserController.java
@@ -1,13 +1,13 @@
 package com.nitin.userservice.controller;
 
+import com.nitin.userservice.api.request.LoginRequest;
 import com.nitin.userservice.api.request.RegisterUser;
 import com.nitin.userservice.api.response.GeneralResponse;
+import com.nitin.userservice.model.UserEntity;
 import com.nitin.userservice.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("user")
@@ -16,8 +16,36 @@ public class UserController {
     @Autowired
     UserService userService;
 
+    /**
+     * Controller Method of Registering the User
+     */
     @PostMapping("register")
     public ResponseEntity<GeneralResponse> registerUser(RegisterUser registerUser) {
         return userService.registerUser(registerUser);
+    }
+
+    /**
+     * Controller Method of Login the User
+     */
+    @PostMapping("login")
+    public ResponseEntity<GeneralResponse> loginUser(@RequestBody LoginRequest loginRequest){
+        return userService.loginUser(loginRequest);
+    }
+
+    /**
+     * Controller Method of Update the User Details
+     */
+    @PutMapping("updateUser/{userId}")
+    public ResponseEntity<GeneralResponse> updateUserDetails(
+            @PathVariable Integer userId, @RequestBody UserEntity userData) {
+        return userService.updateUserDetails(userId, userData);
+    }
+
+    /**
+     * Controller Method of Delete the User
+     */
+    @DeleteMapping("deleteUser/{userId}")
+    public ResponseEntity<GeneralResponse> deleteUser(@PathVariable Integer userId) {
+        return userService.deleteUserById(userId);
     }
 }
--- userservice/src/main/java/com/nitin/userservice/service/UserService.java
+++ userservice/src/main/java/com/nitin/userservice/service/UserService.java
@@ -1,12 +1,20 @@
 package com.nitin.userservice.service;
 
+import com.nitin.userservice.api.request.LoginRequest;
 import com.nitin.userservice.api.request.RegisterUser;
 import com.nitin.userservice.api.response.GeneralResponse;
+import com.nitin.userservice.model.UserEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface UserService {
 
-    public ResponseEntity<GeneralResponse> registerUser(RegisterUser registerUser);
+    ResponseEntity<GeneralResponse> registerUser(RegisterUser registerUser);
+
+    ResponseEntity<GeneralResponse> loginUser(LoginRequest loginRequest);
+
+    ResponseEntity<GeneralResponse> updateUserDetails(Integer userId, UserEntity userData);
+
+    ResponseEntity<GeneralResponse> deleteUserById(Integer userId);
 }
