f6e2098ff23c46a44849fb4ccc70562667912876
Nitin Patil <pnitin372@gmail.com>
2024-10-13 10:08:16 +0530

    Hotel filter implement

--- hotelservice/src/main/java/com/travelbuddy/hotelservice/api/request/FilterParamRequest.java
+++ hotelservice/src/main/java/com/travelbuddy/hotelservice/api/request/FilterParamRequest.java
@@ -6,4 +6,5 @@ import lombok.Data;
 public class FilterParamRequest {
     private String filterField;
     private String filterValue;
+    private String filterCondition;
 }
--- hotelservice/src/main/java/com/travelbuddy/hotelservice/repository/HotelRepositoryInterface.java
+++ hotelservice/src/main/java/com/travelbuddy/hotelservice/repository/HotelRepositoryInterface.java
@@ -2,6 +2,8 @@ package com.travelbuddy.hotelservice.repository;
 
 import com.travelbuddy.hotelservice.model.HotelEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,4 +11,14 @@ import java.util.List;
 @Repository
 public interface HotelRepositoryInterface extends JpaRepository<HotelEntity, Long> {
     HotelEntity findByHotelCode(String hotelCode);
+
+    List<HotelEntity> findByRating(Double rating);
+
+    List<HotelEntity> findByCity(String filterValue);
+
+//    @Query("SELECT h FROM HotelEntity h WHERE h.cost > :cost")
+//    List<HotelEntity> findHotelsByCostGreaterThan(@Param("costValue") Double cost);
+//
+//    @Query("SELECT h FROM HotelEntity h WHERE h.cost < :cost")
+//    List<HotelEntity> findHotelsByCostLessThan(@Param("costValue") Double cost);
 }
