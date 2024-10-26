e4b4348ce9ba9adf17b44c7f4d86394fb72c53f2
Nitin Patil <pnitin372@gmail.com>
2024-10-26 23:43:21 +0530

    changes

--- userservice/src/main/java/com/travelbuddy/userservice/constants/UserServiceConstants.java
+++ userservice/src/main/java/com/travelbuddy/userservice/constants/UserServiceConstants.java
@@ -4,4 +4,7 @@ public class UserServiceConstants {
     public static final String SUCCESS = "success";
     public static final String FAILURE = "failed";
     public static final String ERROR = "error";
+    public static final String TEST = "test";
+
+
 }
